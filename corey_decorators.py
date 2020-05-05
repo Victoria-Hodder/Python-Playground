65764a0570882b3f5c578a599417b0a2b27b45f4
vrhodder <vrhodder@posteo.net>
2020-05-05 16:35:34 +0200

    continuing decorators video, running into some type errors with args/kwargs

--- corey_decorators.py
+++ corey_decorators.py
@@ -4,11 +4,21 @@ Resource: https://www.youtube.com/watch?v=FsAPt_9Bf3U&list=WL&index=14
 
 """
 
-def outer_function():
-    message = 'Hi'
+def decorator_function(original_function):
+    def wrapper_function(*args, **kwargs):
+        print(f'wrapper executed before {original_function.__name__}')
+        return original_function(*args, **kwargs)
+    return wrapper_function()
 
-    def inner_function():
-        print(message)
-    return inner_function()
+@decorator_function
+def display():
+    print('display function ran')
 
-outer_function()
+@decorator_function
+def display_info(name, age):
+    print(f'display_info ran with arguments {name} {age}')
+
+
+display_info('Victoria', 92)
+
+print(display)
