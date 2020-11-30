02c0cdfb262d2c4d2fe99b185fa7843836545a41
vrhodder <vrhodder@posteo.net>
2020-11-30 16:37:03 +0100

    adding questions and notes

--- mt_or_et_10.py
+++ mt_or_et_10.py
@@ -18,23 +18,26 @@ A. Given a list with dictionaries of the form [{'name': 'Mary', 'grade': 4},
 {'name': 'Sissi', 'grade': 19}] and a threshold number, make a function to return 
 a list of dictionaries with the student that have grade more than the number.
 
-
-1. print list of dicts
-2. hwo to access grade key
-
 B. On top of the A solution. Use that function to get the passing students. Then 
 print the passing students names (not inside the function of A, after it is used).
 
 """
 
-def return_dictlist(thisdictlist, threshold):
+# return list of dictionaries
+def return_dictlist(thisdictlist):
+   for value in enumerate(thisdictlist):
+      print(value)
+
+return_dictlist([{'name': 'Mary', 'grade': 4}, {'name': 'Sissi', 'grade': 19}])
+
+# return key-value pair of students who have a grade higher than threshold
+def return_passgrades(thisdictlist, threshold):
     for value in thisdictlist:
         if value['grade'] >= threshold:
             print(f"{value['name']} passed with score {value['grade']}")
-        
 
-return_dictlist([{'name': 'Mary', 'grade': 4}, {'name': 'Sissi', 'grade': 19}, {'name': 'Frank', 'grade': 14}], 15)
+return_passgrades([{'name': 'Mary', 'grade': 4}, {'name': 'Sissi', 'grade': 19}, {'name': 'Frank', 'grade': 14}], 15)
 
-return_dictlist([{'name': 'Mary', 'grade': 4}, {'name': 'Sissi', 'grade': 19}, {'name': 'Frank', 'grade': 14}], 20)
+return_passgrades([{'name': 'Mary', 'grade': 4}, {'name': 'Sissi', 'grade': 19}, {'name': 'Frank', 'grade': 14}], 20)
 
-return_dictlist([{'name': 'Mary', 'grade': 4}, {'name': 'Sissi', 'grade': 19}, {'name': 'Frank', 'grade': 14}], 5)
+return_passgrades([{'name': 'Mary', 'grade': 4}, {'name': 'Sissi', 'grade': 19}, {'name': 'Frank', 'grade': 14}], 5)
